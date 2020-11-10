0f127a716331cee717811e8ffa6d35a6b6878661
kvadropool <kvadropool@gmail.com>
2020-11-10 09:03:32 +0300

    difficult test added

--- test.cpp
+++ test.cpp
@@ -1,16 +1,20 @@
 #include <gtest/gtest.h>
 #include "solve.h"
 
-TEST(OnlyMulTest, OnlyMul) {
+TEST(EasyestTest, TestingOnlyMul) {
 	EXPECT_EQ(4, solve("baac...", 'a', 2));
 }
 
-TEST(MulAndSumTest, MulAndSumTest) {
+TEST(GreaterTest, TestingMulAndSum) {
 	EXPECT_EQ(4, solve("baac...caaab....+", 'a', 2));
 	EXPECT_EQ(INF, solve("baac...caaab....+", 'a', 4));
 }
 
-TEST(Samples, Samples) {
+TEST(Samples, TestsFromSamples) {
 	EXPECT_EQ(4, solve("ab+c.aba.*.bac.+.+*", 'b', 2));
 	EXPECT_EQ(7, solve("acb..bab.c.*.ab.ba.+.+*a.", 'b', 3));
+}
+
+TEST(DifficultTest, TestingKlini) {
+    EXPECT_EQ(9, solve("bbaaaaaaacc..........ab.ba.aaaaa....++*+", 'a', 7));
 }
